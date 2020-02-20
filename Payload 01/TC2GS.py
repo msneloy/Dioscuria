d6f0567af97d37b11cd8ab580b62e29b199736f7
Sierra117 <sierra117@qq.com>
2020-02-20 10:23:36 +0800

    GS JPG

--- Payload 01/TC2GS.py
+++ Payload 01/TC2GS.py
@@ -2,7 +2,7 @@ import cv2
 cv2.namedWindow("Color Image")
 im = cv2.imread('TC.jpg')
 gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-cv2.imwrite('GS.png', gray)
+cv2.imwrite('GS.jpg', gray)
 cv2.imshow("Color Image", im)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
