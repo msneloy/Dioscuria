c1c76719a3b2717229891ca1a413687fa06a3884
Mahadi Sajjad Neloy <sierra117@qq.com>
2020-04-01 13:34:37 +0800

    Cleanup

--- Payload 06/TH.py
+++ Payload 06/TH.py
@@ -6,13 +6,11 @@ im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(im, (5, 5), 0)
 cv2.imwrite("target.jpg", im)
 
-(T, threshInv) = cv2.threshold(blurred, 155, 255, cv2.
-THRESH_BINARY_INV)
-cv2.imwrite("simple thresholding.jpg", cv2.bitwise_and(im, im, mask =
-threshInv))
+(T, threshInv) = cv2.threshold(blurred, 155, 255, cv2.THRESH_BINARY_INV)
+cv2.imwrite("simple thresholding.jpg", cv2.bitwise_and(im, im, mask=threshInv))
 
-thresh = cv2.adaptiveThreshold(blurred, 255,
-cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, 3)
+thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                               cv2.THRESH_BINARY_INV, 15, 3)
 cv2.imwrite("adaptive thresholding.jpg", thresh)
 
 thresh = im.copy()
